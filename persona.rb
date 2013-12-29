ff50cade3473886762fefdf120cf4b10c87897ff
Ayax50000 <fjlsoto@gmail.com>
2013-12-29 13:14:46 -0600

    class Persona defined

---
+++ persona.rb
@@ -0,0 +1,17 @@
+class Persona
+  def initialize(nombre)
+   @nombre = formater! nombre
+  end
+  def registrar(nombre)
+
+  end
+  def asignar(nombre,materia)
+
+  end
+  def existe?(nombre)
+
+  end
+  def ver_asignaciones!(nombre)
+
+  end
+end
