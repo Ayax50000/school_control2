380d1118cfed30675f7e53385960d98c458f0900
Ayax50000 <fjlsoto@gmail.com>
2013-12-29 13:34:24 -0600

    modulo formato done

--- formatear.rb
+++ formatear.rb
@@ -1,8 +1,12 @@
 module Formato
   def formatear_nombre!(nombre)
-
+    capitalizado = ""
+    frase = nombre.split
+    frase.each { |palabra| capitalizado << palabra.capitalize + " "}
+    capitalizado
   end
   def formatear_materia!(materia)
-
+    capitalizado = formatear_nombre! materia
+    capitalizado.gsub(" ","").to_sym
   end
 end
